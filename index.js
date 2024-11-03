5e379c1fc2a5ffed704880380243dce789b43739
Fridgemann <120120633+Fridgemann@users.noreply.github.com>
2024-11-03 21:29:53 +0300

    working on question functionality

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const answers = document.querySelector('.answers');
+const question = document.querySelector('.question');
+const questions = {
+    "Who invented the World Wide Web?" : [
+        "Tim Berners-Lee", "Bill Gates", "Steve Jobs" , "Mark Zuckerberg"
+    ],
+    "What is the rarest naturally occuring element on Earth?" : [
+        "Francium", "Astatine", "Promethium" , "Respiration"
+    ]
+};
+
+const rightAnswers = ["Tim Berners-Lee", "Francium"];
 
 answers.addEventListener('click', (event) => {
     if (event.target !== answers) {
@@ -8,3 +19,16 @@ answers.addEventListener('click', (event) => {
         }
     }
 });
+
+// find a way to display different questions
+
+// question number
+
+// background color change to green on true / red on false
+
+// we need a next button
+
+// add the next button as a div to quiz-app container
+
+/* an array with right answers, if user's answer is included in the array
+its correct */
